b71a9fe1121ade18d7760b019bba84d9cdc472ef
wjyan <wjyan@thoughtworks.com>
2020-02-28 20:45:19 +0800

    [gilded-rose] introduce variable item and method isProductOf

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,54 +13,59 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(PRODUCT_AGED_BRIE)
-                    && !items[i].name.equals(PRODUCT_BACKSTAGE_PASSES)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(PRODUCT_SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
+            Item item = items[i];
+            if (!isProductOf(item, PRODUCT_AGED_BRIE)
+                    && !isProductOf(item, PRODUCT_BACKSTAGE_PASSES)) {
+                if (item.quality > 0) {
+                    if (!isProductOf(item, PRODUCT_SULFURAS)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(PRODUCT_BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (isProductOf(item, PRODUCT_BACKSTAGE_PASSES)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(PRODUCT_SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isProductOf(item, PRODUCT_SULFURAS)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(PRODUCT_AGED_BRIE)) {
-                    if (!items[i].name.equals(PRODUCT_BACKSTAGE_PASSES)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(PRODUCT_SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!isProductOf(item, PRODUCT_AGED_BRIE)) {
+                    if (!isProductOf(item, PRODUCT_BACKSTAGE_PASSES)) {
+                        if (item.quality > 0) {
+                            if (!isProductOf(item, PRODUCT_SULFURAS)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = 0;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private boolean isProductOf(Item item, String name) {
+        return item.name.equals(name);
     }
 }
